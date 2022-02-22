ad8e1f4df371a5a247a7885f6748f52840aa0047
KINASHI <58301322+Kinashi-Naoto@users.noreply.github.com>
2022-02-23 02:05:34 +0900

    vote one time

--- src/components/vote-buttons.js
+++ src/components/vote-buttons.js
@@ -1,29 +1,76 @@
 import { IconButton, Text, VStack } from "@chakra-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FiArrowDown, FiArrowUp } from "react-icons/fi";
 import db from "../lib/firebase";
 
 const VoteButtons = ({ post }) => {
-  const handleClick = async (type) => {
-    // Do calculation to save the vote.
-    let upVotesCount = post.upVotesCount;
-    let downVotesCount = post.downVotesCount;
+    const [isVoting, setVoting] = useState(false);
+    const [votedPosts, setVotedPosts] = useState([]);
+  
+    useEffect(() => {
+      // Fetch the previously voted items from localStorage. See https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.parse" and update the item on localStorage. Return "true" if the user has already voted the post.
+      const votesFromLocalStorage = localStorage.getItem("votes") || [];
+      let previousVotes = [];
+  
+      try {
+        // Parse the value of the item from localStorage. If the value of the
+        // items isn't an array, then JS will throw an error.
+        previousVotes = JSON.parse(votesFromLocalStorage);
+      } catch (error) {
+        console.error(error);
+      }
+  
+      setVotedPosts(previousVotes);
+    }, []);
+  
+    const handleDisablingOfVoting = (postId) => {
+      // This function is responsible for disabling the voting button after a
+      // user has voted. Fetch the previously voted items from localStorage. See
+      // https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.parse"
+      // and update the item on localStorage.
+      const previousVotes = votedPosts;
+      previousVotes.push(postId);
+  
+      setVotedPosts(previousVotes);
+  
+      // Update the voted items from localStorage. See https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.stringify" and update the item on localStorage.
+      localStorage.setItem("votes", JSON.stringify(votedPosts));
+    };
 
-    const date = new Date();
+    const handleClick = async (type) => {
+        // Do calculation to save the vote.
+        setVoting(true);
+        let upVotesCount = post.upVotesCount;
+        let downVotesCount = post.downVotesCount;
+    
+        const date = new Date();
+    
+        if (type === "upvote") {
+          upVotesCount = upVotesCount + 1;
+        } else {
+          downVotesCount = downVotesCount + 1;
+        }
+    
+        await db.collection("posts").doc(post.id).set({
+          title: post.title,
+          upVotesCount,
+          downVotesCount,
+          createdAt: post.createdAt,
+          updatedAt: date.toUTCString(),
+        });
+        // Disable the voting button once the voting is successful.
+        handleDisablingOfVoting(post.id);
 
-    if (type === "upvote") {
-      upVotesCount = upVotesCount + 1;
+        setVoting(true);
+      };
+ 
+
+  const checkIfPostIsAlreadyVoted = () => {
+    if (votedPosts.indexOf(post.id) > -1) {
+      return true;
     } else {
-      downVotesCount = downVotesCount + 1;
+      return false;
     }
-
-    await db.collection("posts").doc(post.id).set({
-      title: post.title,
-      upVotesCount,
-      downVotesCount,
-      createdAt: post.createdAt,
-      updatedAt: date.toUTCString(),
-    });
   };
 
   return (
@@ -35,6 +82,8 @@ const VoteButtons = ({ post }) => {
           aria-label="Upvote"
           icon={<FiArrowUp />}
           onClick={() => handleClick("upvote")}
+          isLoading={isVoting}
+          isDisabled={checkIfPostIsAlreadyVoted()}
         />
         <Text bg="gray.100" rounded="md" w="100%" p={1}>
           {post.upVotesCount}
@@ -47,6 +96,8 @@ const VoteButtons = ({ post }) => {
           aria-label="Downvote"
           icon={<FiArrowDown />}
           onClick={() => handleClick("downvote")}
+          isLoading={isVoting}
+          isDisabled={checkIfPostIsAlreadyVoted()}
         />
         <Text bg="gray.100" rounded="md" w="100%" p={1}>
           {post.downVotesCount}
